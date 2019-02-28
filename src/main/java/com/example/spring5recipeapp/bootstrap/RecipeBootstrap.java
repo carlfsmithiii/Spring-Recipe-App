993d9daa0d3355d214aeed6f7235ea4a8b5b053f
Carl Smith <carlfsmithiii@gmail.com>
2019-02-28 13:24:29 -0500

    partial addition of bootstrap class

---
+++ src/main/java/com/example/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -0,0 +1,89 @@
+package com.example.spring5recipeapp.bootstrap;
+
+import com.example.spring5recipeapp.domain.Category;
+import com.example.spring5recipeapp.domain.Difficulty;
+import com.example.spring5recipeapp.domain.Recipe;
+import com.example.spring5recipeapp.domain.UnitOfMeasure;
+import com.example.spring5recipeapp.repositories.CategoryRepository;
+import com.example.spring5recipeapp.repositories.RecipeRepository;
+import com.example.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeInvisAnnos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RecipeBootstrap {
+
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    private List<Recipe> getRecipes() {
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+
+        if (!eachUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> tableSpoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+        if (!tableSpoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> teaSpoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        if (!teaSpoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+        if (!dashUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+        if (!pintUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> cupsUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        if (!cupsUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tableSpoonUom = tableSpoonUomOptional.get();
+        UnitOfMeasure teaSpoonUom = teaSpoonUomOptional.get();
+        UnitOfMeasure dashUom = dashUomOptional.get();
+        UnitOfMeasure pintUom = pintUomOptional.get();
+        UnitOfMeasure cupsUom = cupsUomOptional.get();
+
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+        if (!americanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+        if (!mexicanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found");
+        }
+
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacamole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setDirections();
+    }
+}
